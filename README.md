06c1be9c85186997809627a66c84f38b7ebb9242
yujuiting <yujuiting@me.com>
2016-03-14 21:51:22 +0800

    Add QuadTree.Boundary. Implement QuadTree.Node.findItemsIn

--- README.md
+++ README.md
@@ -23,6 +23,16 @@
     QuadTree.Pointer.SouthEast
     QuadTree.Pointer.SouthWest
 
+
+
+    struct QuadTree.Node {
+        level: Number
+        boundary: QuadTree.Boundary
+        children: Array<QuadTree.Node>
+        items: Array<QuadTree.Item>
+        parent: QuadTree.Node?
+    }
+
     QuadTree.Node.create(level:Number, x:Number, y:Number, width:Number, height:Number[, parent:QuadTree.Node]) => QuadTree.Node
 
     QuadTree.Node.insert(node:QuadTree.Node, item:QuadTree.Item) => void
@@ -31,6 +41,33 @@
 
     QuadTree.Node.contain(node:QuadTree.Node, x:Number, y:Number) => Boolean
 
-    QuadTree.Node.getChildByPoint(node:QuadTree.Node, x:Number, y:Number) => QuadTree.Node?
+    QuadTree.Node.getChildAt(node:QuadTree.Node, x:Number, y:Number) => QuadTree.Node?
+
+    QuadTree.Node.findItem(node:QuadTree.Node, x:Number, y:Number, tolerance:Number) => QuadTree.Item?
+
+    QuadTree.Node.findItemsIn(node:QuadTree.Node, boundary:QuadTree.Boundary) => Array<QuadTree.Item>
+
+
+
+    struct QuadTree.Item {
+        x: Number
+        y: Number
+        data: Any?
+    }
 
     QuadTree.Item.create(x:Number, y:Number, data:Any) => QuadTree.Item
+
+
+
+    struct QuadTree.Boundary {
+        x: Number
+        y: Number
+        width: Number
+        height: Number
+    }
+
+    QuadTree.Boundary.create(x:Number, y:Number, width:Number, height:Number) => QuadTree.Boundary
+
+    QuadTree.Boundary.contain(boundary:QuadTree.Boundary, x:Number, y:Number) => Boolean
+
+    QuadTree.Boundary.intersect(boundary1:QuadTree.Boundary, boundary2:QuadTree.Boundary) => Boolean
---
+++ quadtree.js
@@ -0,0 +1,178 @@
+(function (env, def) {
+
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = def();
+  } else {
+    env.QuadTree = def();
+  }
+
+})(this, function () {
+  QuadTree = {};
+
+  QuadTree.MaxItemCount = 5;
+  QuadTree.MaxChildrenCount = 4;
+
+  QuadTree.Pointer = {
+    NorthEast: 0, // +,-
+    NorthWest: 1, // -,-
+    SouthEast: 2, // +,+
+    SouthWest: 3  // -,+
+  };
+
+  QuadTree.Node = {
+    create: function (level, x, y, width, height, parent) {
+      return {
+        level: level,
+        boundary: QuadTree.Boundary.create(x, y, width, height),
+        children: [],
+        items: [],
+        parent: parent
+      };
+    },
+    insert: function (node, item) {
+      if (node.children.length) {
+        var childNode = QuadTree.Node.getChildAt(node, item.x, item.y);
+        QuadTree.Node.insert(childNode, item);
+      } else {
+        node.items.push(item);
+        if (node.items.length > QuadTree.MaxItemCount) {
+          QuadTree.Node.split(node);
+        }
+      }
+    },
+    split: function (node) {
+      var nodeItems = node.items,
+          cnLevel = node.level + 1,
+          cnWith = node.boundary.width / 2,
+          cnHeight = node.boundary.height / 2,
+          offsetX = cnWith / 2,
+          offsetY = cnHeight / 2,
+          cnX, cnY, i, j, itemCount, item;
+      for (i = 0; i < QuadTree.MaxChildrenCount; i++) {
+        switch (i) {
+          case QuadTree.Pointer.NorthEast:
+            cnX = node.boundary.x + offsetX;
+            cnY = node.boundary.y - offsetY;
+            break;
+          case QuadTree.Pointer.NorthWest:
+            cnX = node.boundary.x - offsetX;
+            cnY = node.boundary.y - offsetY;
+            break;
+          case QuadTree.Pointer.SouthEast:
+            cnX = node.boundary.x + offsetX;
+            cnY = node.boundary.y + offsetY;
+            break;
+          case QuadTree.Pointer.SouthWest:
+            cnX = node.boundary.x - offsetX;
+            cnY = node.boundary.y + offsetY;
+            break;
+        }
+        node.children[i] = QuadTree.Node.create(cnLevel, cnX, cnY, cnWith, cnHeight, node);
+        itemCount = node.items.length;
+        for (j = itemCount - 1; j >= 0; j--) {
+          item = node.items[j];
+          if (QuadTree.Node.contain(node.children[i], item.x, item.y)) {
+            node.children[i].items.push(item);
+            node.items.splice(j, 1);
+          }
+        }
+      }
+    },
+    contain: function (node, x, y) {
+      return QuadTree.Boundary.contain(node.boundary, x, y);
+    },
+    getChildAt: function (node, x, y) {
+      if (!node.children.length) {
+        return null;
+      }
+      if (x >= node.boundary.x) {
+        if (y >= node.boundary.y) {
+          return node.children[QuadTree.Pointer.SouthEast];
+        } else {
+          return node.children[QuadTree.Pointer.NorthEast];
+        }
+      } else {
+        if (y >= node.boundary.y) {
+          return node.children[QuadTree.Pointer.SouthWest];
+        } else {
+          return node.children[QuadTree.Pointer.NorthWest];
+        }
+      }
+      return null;
+    },
+    findItem: function (node, x, y, tolerance) {
+      if (!QuadTree.Node.contain(node, x, y))
+        return null;
+      var i, item, count = node.items.length;
+      for (i = 0; i < count; i++) {
+        item = node.items[i];
+        if (distance(item.x, item.y, x, y) < tolerance) {
+          return item;
+        }
+      }
+      var childNode = QuadTree.Node.getChildAt(node, x, y);
+      if (childNode)
+        return QuadTree.Node.findItem(childNode, x, y, tolerance);
+
+      return null;
+    },
+    findItemIn: function (node, boundary) {
+      var i, item, items = [];
+      if (!QuadTree.Boundary.intersect(node.boundary, boundary))
+        return items;
+      for (i = 0; i < node.children.length; i++)
+        items = items.concat(QuadTree.Node.findItemIn(node.children[i], boundary));
+      for (i = 0; i < node.items.length; i++) {
+        item = node.items[i];
+        if (QuadTree.Boundary.contain(boundary, item.x, item.y))
+          items.push(node.items[i]);
+      }
+      return items;
+    }
+  };
+
+  QuadTree.Item = {
+    create: function (x, y, data) {
+      return { x: x, y: y, data: data };
+    }
+  };
+
+  QuadTree.Boundary = {
+    create: function (x, y, width, height) {
+      return {x: x, y: y, width: width, height: height};
+    },
+    contain: function (boundary, x, y) {
+      var nodeHalfWidth = boundary.width / 2,
+          nodeHalfHeight = boundary.height / 2;
+      if (x <  boundary.x -  nodeHalfWidth)
+        return false;
+      if (x >= boundary.x + nodeHalfWidth)
+        return false;
+      if (y <  boundary.y - nodeHalfHeight)
+        return false;
+      if (y >= boundary.y + nodeHalfHeight)
+        return false;
+      return true;
+    },
+    intersect: function (boundary1, boundary2) {
+      var halfWidth1  = boundary1.width / 2,
+          halfHeight1 = boundary1.height / 2,
+          halfWidth2  = boundary2.width / 2,
+          halfHeight2 = boundary2.height / 2;
+      if ((boundary1.x + halfWidth1 < boundary2.x - halfWidth2)||
+          (boundary1.x - halfWidth1 > boundary2.x + halfWidth2))
+        return false;
+      if ((boundary1.y + halfHeight1 < boundary2.y - halfHeight2)||
+          (boundary1.y - halfHeight1 > boundary2.y + halfHeight2))
+        return false;
+      return true;
+    }
+  };
+
+  // Helper
+  function distance (x1, y1, x2, y2) {
+    return Math.pow(Math.pow(x2 - x1 , 2) + Math.pow(y2 - y1 , 2) , 0.5);
+  }
+
+  return QuadTree;
+});
